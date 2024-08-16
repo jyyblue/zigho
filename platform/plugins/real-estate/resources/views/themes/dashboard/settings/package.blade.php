71df279d878b91c8f620cee6991c95a54231f4e7
wenqiang <wenqiangliu344@gmail.com>
2024-08-16 16:10:49 +0800

    update

--- platform/plugins/real-estate/resources/views/themes/dashboard/settings/package.blade.php
+++ platform/plugins/real-estate/resources/views/themes/dashboard/settings/package.blade.php
@@ -44,7 +44,7 @@
                                     <li v-if="!item.price">&nbsp;</li>
                                 </ul>
                                 <div class="text-center mt-4">
-                                    <x-core::button v-if="item.price"
+                                    <x-core::button v-if="item.price && item.id != 1"
                                         class="w-100"
                                         v-bind:class="isSubscribing && currentPackageId === item.id ? 'btn btn-success button-loading mt-2' : (item.is_default ? 'btn btn-success mt-2' : 'btn mt-2')"
                                         v-on:click="postSubscribe(item.id)"
